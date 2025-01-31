68f8369e5d4178d114e7a61681b889aafb887fd1
ARP2112 <121040859@upq.edu.mx>
2025-01-31 09:43:23 -0600

    Imprimir si la cadena ingresada es Palindromo o no

--- Practica 4/src/Palindromo.java
+++ Practica 4/src/Palindromo.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
+
 public class Palindromo {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce una cadena de texto: ");
+        String cadena = scanner.nextLine();
+
+        String cadenaL = cadena.replaceAll("\\s+", "").toLowerCase();
+
+        if (esPalindromo(cadenaL)) {
+            System.out.println("La cadena es un palindromo.");
+        }
+        else {
+            System.out.println("La cadena NO ES UN PALINDROMO.");
+        }
+    }
+    public static boolean esPalindromo(String str) {
+        int inicio = 0;
+        int fin = str.length() - 1;
+
+        while (inicio < fin) {
+            if (str.charAt(inicio) != str.charAt(fin)) {
+                return false;
+            }
+            inicio++;
+            fin--;
+        }
+        return true;
+    }
+
+        }
+
+
