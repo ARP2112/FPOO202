09ed114f44a7a9aa55d11b728b02f50600016d48
ARP2112 <121040859@upq.edu.mx>
2025-02-21 19:18:44 -0600

    Creación de un personaje
    Utilizando una Clase en Java y ejecutando desde el Main

---
+++ PASSWORD/src/Password.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Password {
+    public String password;
+
+    Password(int length, boolean includeUppercase, boolean includeSpecials){
+        this.password = generatePassword(length, includeUppercase, includeSpecials);
+    }
+    public String generatePassword (int length, boolean includeUppercase, boolean includeSpecials){
+        
+    }
+
+
+}
